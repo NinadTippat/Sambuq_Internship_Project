5dc80bff4f81e0dd7109cdbb603eec1b7f3df8af
Ninad Tippat <tippatninad17@gmail.com>
2024-01-02 15:19:33 +0530

    second

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ db.once('open', () => {
 });
 
 
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/public")));
 
 app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/public/index.html'));
 });
 
 
